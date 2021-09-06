ac45b8081575c92bc5d888e6f05d143e9246860d
Gourunze <gourunze@xiaomi.com>
2021-09-06 20:13:09 +0800

    try to find initial feasible solution

--- Code/dataOrganising.py
+++ Code/dataOrganising.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime as dt
 
 
-games = pd.read_excel('Data/Round4_LP.xlsx', engine='openpyxl')
+games = pd.read_excel('~/mast90050Project/Code/Data/Round4_LP.xlsx', engine='openpyxl')
 games = games.rename(columns={"Unnamed: 8": "Field.2"})
 games["Field"] = np.nan
 games["Field.1"] = np.nan
@@ -11,10 +11,10 @@ games["Field.2"] = np.nan
 games["Boundary"] = np.nan
 games["Boundary.1"] = np.nan
 
-umps = pd.read_excel('Data/LP_Umps2021.xlsx', engine='openpyxl')
-availability = pd.read_excel('Data/LP_Umps2021.xlsx', sheet_name="Availability",
+umps = pd.read_excel('~/mast90050Project/Code/Data/LP_Umps2021.xlsx', engine='openpyxl')
+availability = pd.read_excel('~/mast90050Project/Code/Data/LP_Umps2021.xlsx', sheet_name="Availability",
                              usecols="A,E", nrows=118, engine="openpyxl")
-playingUmps = pd.read_excel('Data/Lp_playing_Rd4.xlsx', usecols="A,D",
+playingUmps = pd.read_excel('~/mast90050Project/Code/Data/LP_playing_Rd4.xlsx', usecols="A,D",
                              engine="openpyxl")
 
 umpMaster = umps
@@ -28,29 +28,30 @@ playingUmps = playingUmps.set_index("Name")
         
 # Assign availability to playing umps        
 # Dictionary    playing time : [allowable umpiring times]
-allTimes = [dt.time(8,30), dt.time(10,0), dt.time(11,0), dt.time(11,30),
-            dt.time(13,0), dt.time(14,30), dt.time(15,0)]
+allTimes = ['8:30:00', '10:00:00', '11:00:00', '11:30:00',
+            '13:00:00', '14:30:00', '15:00:00']
 playAllow = {
-    dt.time(8,30): [dt.time(11,0), dt.time(11,30), 
-            dt.time(13,0), dt.time(14,30), dt.time(15,0)],
-    dt.time(10,0): [dt.time(13,0), dt.time(14,30), 
-                          dt.time(15,0)],
-    dt.time(11,0): [dt.time(8,30), dt.time(14,30), 
-                          dt.time(15,0)],
-    dt.time(11,30): [dt.time(8,30), dt.time(14,30), 
-                          dt.time(15,0)],
-    dt.time(13,0): [dt.time(8,30), dt.time(10,0)],
-    dt.time(14,30): [dt.time(8,30), dt.time(10,0),
-                          dt.time(11,30)],
-    dt.time(15,0): [dt.time(8,30), dt.time(10,0),
-                          dt.time(11,30)]
+    '8:30:00': ['11:00:00', '11:30:00',
+            '13:00:00', '14:30:00', '15:00:00'],
+    '10:00:00': ['13:00:00', '14:30:00',
+                          '15:00:00'],
+    '11:00:00': ['8:30:00', '14:30:00',
+                          '15:00:00'],
+    '11:30:00': ['8:30:00', '14:30:00',
+                          '15:00:00'],
+    '13:00:00': ['8:30:00', '10:00:00'],
+    '14:30:00': ['8:30:00', '10:00:00',
+                          '11:30:00'],
+    '15:00:00': ['8:30:00', '10:00:00',
+                          '11:30:00']
     }
 
 for entry in playingUmps.index:
     if entry in umpAvail.index:
         playtime = playingUmps.loc[entry][0]
 
-        if type(playtime) != type(np.nan):
+        if type(playtime) != type(np.nan) and playtime != None:
+            playtime = playtime.strftime("%H:%M:%S")
             times = playAllow[playtime]
         else:
             times = allTimes     
@@ -59,6 +60,7 @@ for entry in playingUmps.index:
         playtime = playingUmps.loc[entry][0]
 
         if type(playtime) != type(np.nan):
+            playtime = playtime.strftime("%H:%M:%S")
             times = playAllow[playtime]
         else:
             times = allTimes
@@ -87,12 +89,15 @@ for entry in umpAvail.index:
     avail = umpAvail.loc[entry][0]
     if type(avail) == type(np.nan):
         umpAvail.loc[entry][0] = allTimes
+umpMaster = umpMaster[0:120]
+umpAvail = umpAvail[0:120]
+
 
 print("\n******\n")
 print(np.all(umpMaster.index == umpAvail.index))
 #%% Assign umpires to clubs they play for
     
-team = [np.nan for i in range(umpMaster.shape[0])]
+team = [' ' for i in range(umpMaster.shape[0])]
 umpMaster["Club"] = team
 
 for ump in umpMaster.index:
@@ -112,15 +117,15 @@ umpMaster.loc["Boulter, Oscar"]["2 Games"] = False
 umpAvail.loc["Boulter, Oscar"][0] = allTimes
 umpAvail.loc["Collopy, John"][0] = allTimes[:-1]
 umpMaster.loc["Lee, Andrew"]["2 Games"] = False
-umpAvail.loc["Stacey, Joshua"][0] = [dt.time(13,0), dt.time(15,0)]
+umpAvail.loc["Stacey, Joshua"][0] = ['13:00:00', '15:00:00']
 
 # changes from 'Additional Notes'
-umpAvail.loc["Anderson, Callum"][0] = [dt.time(13,0), dt.time(15,0)]
-umpAvail.loc["Brown, Henry"][0] = [dt.time(13,0), dt.time(15,0)]
-umpAvail.loc["Dodds, Matthew"][0] = dt.time(15,0)
-umpAvail.loc["Lee, Andrew"][0] = [dt.time(10,0), dt.time(11,30)]
-umpAvail.loc["Scott, Angus"][0] = [dt.time(13,0), dt.time(15,0)]
-umpAvail.loc["Walsh, Austin"][0] = [dt.time(13,0), dt.time(15,0)]
+umpAvail.loc["Anderson, Callum"][0] = ['13:00:00', '15:00:00']
+umpAvail.loc["Brown, Henry"][0] = ['13:00:00', '15:00:00']
+umpAvail.loc["Dodds, Matthew"][0] = ['15:00:00']
+umpAvail.loc["Lee, Andrew"][0] = ['10:00:00', '11:30:00']
+umpAvail.loc["Scott, Angus"][0] = ['13:00:00', '15:00:00']
+umpAvail.loc["Walsh, Austin"][0] = ['13:00:00', '15:00:00']
 #%% Remove unavailable umpires
 for ump in umpAvail.index:
     if umpAvail.loc[ump][0] == "Unavailable":
@@ -140,3 +145,34 @@ umpMaster = umpMaster.rename(columns = {umpAvail.columns[0]: "Available"})
 cols = ["Age Group", "Category", "2 Games", "Available", "Playing", "Club",
         "Additonal Notes", "Last Name", "First Name"]
 umpMaster = umpMaster[cols]
+
+for entry in umpMaster.index:
+    if umpMaster.loc[entry]['Available'] is None:
+        umpMaster.loc[entry]['Available'] = allTimes
+
+#greedy algorithm:
+
+
+u = [[] for i in range(games.shape[0])]
+games['ump'] = u
+games.set_index('Venue Name')
+num = 0
+for i in games.index:
+    team = []
+    time = games.loc[i]['Match Time']
+    time = str(time)
+    Category = games.loc[i]['Category']
+    team.append(games.loc[i]['Team 1'])
+    team.append(games.loc[i]['Team 2'])
+    for j in umpMaster.index:
+        num+=1
+        if umpMaster.loc[j]['Available'] is not None and type(umpMaster.loc[j]['Available']) is not dt.time:
+            if time in umpMaster.loc[j]['Available'] and len(games.loc[i]['ump']) < 1:
+                umpMaster.loc[j]['Available'] = umpMaster.loc[j]['Available'].remove(time)
+                games.loc[i]['ump'].append(j)
+for index in games.index:
+    if games.loc[index]['ump'] == []:
+        print(index)
+
+
+
